6d672268a9a4459b3e2e4ea15e1ff9ff6e0d186e
VishnuKumar006 <JaiShreeRam06@hotmail.com>
2024-06-15 13:08:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// let menulist = document.getElementById("menulist");
+// menulist.style.maxHeight = "0px";
+
+// function toggleMenu() {
+//   if (menulist.style.maxHeight == "0px") {
+//     menulist.style.maxHeight = "300px";
+//   } else {
+//     menulist.style.maxHeight = "0px";
+//   }
+// }
+var typed = new Typed('#element', {
+    strings: ['Welcome to StudySync !', 'Here I will You basic to advanced', 'Coding technique which took me ages to learn', 'You can ask your doubts with our paid community also.', 'here I will teach you Web &amp; App Development', '&amp; some Programming Languages like Html', 'Css, Javascript, C++, C languages, Java, Python', '&amp; DSA (Data Structure &amp; Algorithms)', 'with that courses are free for starting *100*', 'Users so go now &amp; login your account on StudySync !'],
+    typeSpeed: 50,
+    backspeed: 50,
+    loop: true
+});
